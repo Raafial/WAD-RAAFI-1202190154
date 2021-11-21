e6f6674cd5f8b7541b8d9ff57bdea3187e877bcd
Raafial <60471074+Raafial@users.noreply.github.com>
2021-11-21 23:47:34 +0700

    Add files via upload

---
+++ MODUL3 RAAFI/pengaturan/connect.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost:3307";$username = "root";$password = "";$database = "modul3";
+
+$sambung = new mysqli($servername, $username, $password, $database);
+$connect = mysqli_connect($servername, $username, $password, $database);
+
+?>
