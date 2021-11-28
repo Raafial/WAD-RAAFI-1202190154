2eb00035ab6c0ea05ffbe131cc4b5aaa323f9958
Raafial <60471074+Raafial@users.noreply.github.com>
2021-11-28 23:52:48 +0700

    Add files via upload

---
+++ MODUL4 RAAFI/register.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <style>
+        .login {
+            width: 500px;
+            margin-top: 100px;
+            margin-bottom: 100px;
+            overflow: hidden;
+            transition: all ease 1s;
+        }
+    </style>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body style="background-color:#FBECE9;">
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #13CDE3">
+        <a class="navbar-brand" href="#"><b>EAD Travel</b></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <div class="navbar-nav">
+                <li class="nav-item">
+                    <a href="login.php" class="nav-link active">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a href="register.php" class="nav-link active">Register</a>
+                </li>
+            </div>
+
+        </div>
+    </nav>
+    <!-- Navbar -->
+
+
+    <div class="container">
+        <?php
+        include('connect.php');
+        $db = new database();
+
+        if (isset($_POST['register'])) {
+            $db->register(
+                $_POST['name'],
+                $_POST['email'],
+                $_POST['phone'],
+                $_POST['pass-init'],
+                $_POST['pass-conf']
+            );
+        }
+        ?>
+
+    </div>
+
+    <div class="container d-flex justify-content-center" style="height: 100%;">
+        <div class=" login ">
+            <div class="card">
+                <div class="card-body">
+                    <form action="" method="post">
+                        <h1 style="text-align: center;">Register</h1>
+                        <hr>
+                        <div class="form-group">
+                            <label for="">Nama</label>
+                            <input type="text" required class="form-control" name="name" id="" aria-describedby="emailHelpId" placeholder="Masukkan Nama Lengkap">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Email</label>
+                            <input type="text" required class="form-control" name="email" id="" aria-describedby="emailHelpId" placeholder="Masukkan Alamat E-Mail">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">No Handphone</label>
+                            <input type="number" required class="form-control" name="phone" id="" placeholder="Masukkan Nomor Handphone">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Kata Sandi</label>
+                            <input type="password" required class="form-control" name="pass-init" id="" placeholder="Kata Sandi Anda">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Konfirmasi Kata Sandi</label>
+                            <input type="password" required class="form-control" name="pass-conf" id="" placeholder="Konfirmasi Kata Sandi Anda">
+                        </div>
+
+                        <br>
+                        <div class="form-group  text-center">
+                            <button type="submit" name="register" id="" class="btn btn-primary" style="width: 175px" btn-lg btn-block>Daftar</button>
+                        </div>
+                    </form>
+                    
+                    <p style="text-align: center;">Anda sudah punya akun? <a href="login.php">Login</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="bg-light text-center text-lg-start">
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: #13CDE3;">
+                © 2021 Copyright:
+                <a class="text-dark" data-toggle="modal" data-target="#exampleModal">RAAFI ALBAR_1202190154</a>
+            </div>
+            <!-- Copyright -->
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Created By</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Nama : Raafi Albar
+                </div>
+                <div class="modal-body">
+                    NIM  : 1202190154
+                </div>
+            </div>
+    </footer>
+    <!-- Footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/connect/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
